ca29b8a9a0c8282cc27d89abe6ea79550be4e18b
Lorenzo Braschi <lbraschi@gmail.com>
2013-02-26 11:13:35 +0100

    Cambiado if por ifelse en función simCorr

--- simCorrclast.R
+++ simCorrclast.R
@@ -4,9 +4,7 @@ simCorrclast <- function(n,r){
   require(MASS)
   muestra <- mvrnorm(n/2, Sigma = matrix(c(1,r,r,1),2),mu = rep(0,2))
   out.cor <- cor(muestra[,1],muestra[,2])
-  fishZ <- fisherZ(out.cor) 
-  z <- fishZ/(1/sqrt(n-3)) 
-  pvalue <- 1-pnorm(z)
+  tstat <- out.cor*sqrt(n-2)/sqrt(1-out.cor^2)
   while (pvalue<0.25 & pvalue>.01){
     muestra <- rbind(muestra,mvrnorm(1, Sigma = matrix(c(1,r,r,1),2),mu = rep(0,2)))
     out.cor <- cor(muestra[,1],muestra[,2])
