48c8734b06275c46297701d1d419eeeac9f809e0
Lorenzo Braschi <lbraschi@gmail.com>
2013-04-23 16:40:24 +0200

    Gráficos

--- correlaciones.R
+++ correlaciones.R
@@ -8,7 +8,7 @@
 # Condiciones de simulación-----
 library(MASS)
 num.sim <- 10^4 # número de simulaciones
-nfsr <- c(16, 20, 24, 30, 40) # tamaño muestral inicial de referencia
+nfsr <- c(20, 30, 40, 50, 60) # tamaño muestral inicial de referencia
 corr.teo <- c(0,.20,.40,.60,.80)
 
 # Data Frame ----
@@ -21,6 +21,7 @@ datos.corrs$decision <- NA
 rm(num.sim,nfsr,corr.teo)
 # set.seed(1234)
 
+# Bucle de simulación ------
 source(file="simCorr.R")
 # source(file="fisherZ.R")
 
@@ -30,7 +31,16 @@ for (i in 1:dim(datos.corrs)[1]){
 
 rm(i)
 
+# Tabla Resumen -----
+
+datos.corrs$zfisher <- .5*log((1+datos.corrs$corr_emp)/(1-datos.corrs$corr_emp))
+datos.corrs$varteo.zfisher <- 1/(datos.corrs$nfsr-3)
+
 r.corr <- aggregate.data.frame(datos.corrs, by = list(datos.corrs$corr_teo,datos.corrs$nfsr),FUN=mean)
+r.corr$Group.1 <- r.corr$Group.2 <- NULL
+
+# Calcula la varianza empírica de las Z agrupadas por corr_teo y nfsr
+r.corr$varemp.zfisher <- aggregate(datos.corrs$zfisher, by = list(datos.corrs$corr_teo,datos.corrs$nfsr),FUN=var)$x
 
 # Gráficos -----
 library(ggplot2)
@@ -51,16 +61,43 @@ corr.pow <- corr.pow +
   geom_line()+
   theme_bw()+
   scale_x_continuous(name="Correlación teórica")+
-  scale_y_continuous(name="Proporción empírica de rechazos")+
+  scale_y_continuous(name="Proporción empírica de rechazos",limits=c(0,1))+
   scale_shape_discrete(name="N fijo")+
   scale_color_discrete(name="N fijo")+
   scale_linetype_discrete(name="N fijo")+
   geom_hline(yintercept=.05, linetype=2,alpha=.5,size=1)
 ggsave(plot=corr.pow, "/Users/lbraschi/Documents/Investigación/Tesis/simulacion_correlaciones/corrpow.png",height=8, width=12,dpi=600)
 
-histh0 <- ggplot(datos.corrs[datos.corrs$corr_teo==0,],aes(x=tstat,y=..density..))+
-  geom_histogram(binwidth=1/10,fill="steelblue",color="black",prb=T)+
-  theme_bw()+geom_density(color="red",size=1,adjust=2)+
-  theme(plot.title=element_text(size=18))+
-  ggtitle("Histograma H0=VERDADERA, RTP")
-ggsave(plot=histh0, "/Users/lbraschi/Documents/Investigación/Tesis/simulacion_correlaciones/histh0.png",height=8, width=12,dpi=600)
+# histh0 <- ggplot(datos.corrs[datos.corrs$corr_teo==0,],aes(x=tstat,y=..density..))+
+#   geom_histogram(binwidth=1/10,fill="steelblue",color="black",prb=T)+
+#   theme_bw()+geom_density(color="red",size=1,adjust=2)+
+#   theme(plot.title=element_text(size=18))+
+#   ggtitle("Histograma H0=VERDADERA, RTP")
+# ggsave(plot=histh0, "/Users/lbraschi/Documents/Investigación/Tesis/simulacion_correlaciones/histh0.png",height=8, width=12,dpi=600)
+
+corr.varzteo <- ggplot(r.corr, aes(x=corr_teo,y=varteo.zfisher, color=factor(nfsr),shape=factor(nfsr), linetype=factor(nfsr)))
+corr.varzteo <- corr.varzteo + 
+  geom_point(size = 3)+
+  geom_line()+
+  theme_bw()+
+  theme(plot.title = element_text(size = 18, face = "bold"))+
+  scale_x_continuous(name="Correlación teórica")+
+  scale_y_continuous(name="Varianza teórica")+
+  scale_shape_discrete(name="N fijo")+
+  scale_color_discrete(name="N fijo")+
+  scale_linetype_discrete(name="N fijo")+
+  ggtitle("Varianza de Z de Fisher\na partir de los tamaños de parada")
+
+corr.varzemp <- ggplot(r.corr, aes(x=corr_teo,y=varemp.zfisher, color=factor(nfsr),shape=factor(nfsr), linetype=factor(nfsr)))
+corr.varzemp <- corr.varzemp + 
+  geom_point(size = 3)+
+  geom_line()+
+  theme_bw()+
+  theme(plot.title = element_text(size = 18, face = "bold"))+
+  scale_x_continuous(name="Correlación teórica")+
+  scale_y_continuous(name="Varianza empírica")+
+  scale_shape_discrete(name="N fijo")+
+  scale_color_discrete(name="N fijo")+
+  scale_linetype_discrete(name="N fijo")+
+  ggtitle("Varianza de Z de Fisher\ncalculada empíricamente")
+
--- correlaciones_clast.R
+++ correlaciones_clast.R
@@ -8,7 +8,7 @@
 # Condiciones de simulación-----
 library(MASS)
 num.sim <- 10^4 # número de simulaciones
-nfsr <- c(16, 20, 24, 30, 40) # tamaño muestral inicial de referencia
+nfsr <- c(20, 30, 40, 50, 60) # tamaño muestral inicial de referencia
 corr.teo <- c(0,.20,.40,.60,.80)
 
 # Data Frame ----
@@ -22,8 +22,11 @@ datos.corrsclast$decision <- NA
 rm(num.sim,nfsr,corr.teo)
 # set.seed(1234)
 
+# Bucle de simulación ----
+
 source(file="simCorrclast.R")
 # source(file="fisherZ.R")
+
 
 for (i in 1:dim(datos.corrsclast)[1]){
   datos.corrsclast[i,3:7] <- simCorrclast(n = datos.corrsclast$nfsr[i], r = datos.corrsclast$corr_teo[i],asup=.25)
@@ -31,10 +34,21 @@ for (i in 1:dim(datos.corrsclast)[1]){
 
 rm(i)
 
+# Tabla Resumen -----
+
+datos.corrsclast$zfisher <- .5*log((1+datos.corrsclast$corr_emp)/(1-datos.corrsclast$corr_emp))
+datos.corrsclast$varteo.zfisher <- 1/(datos.corrsclast$nstop-3)
+
 r.corrclast <- aggregate.data.frame(datos.corrsclast, by = list(datos.corrsclast$corr_teo,datos.corrsclast$nfsr),FUN=mean)
+r.corrclast$Group.1 <- r.corrclast$Group.2 <- NULL
+
+# Calcula la varianza empírica de las Z agrupadas por corr_teo y nfsr
+
+r.corrclast$varemp.zfisher <- aggregate(datos.corrsclast$zfisher, by=list(datos.corrsclast$corr_teo,datos.corrsclast$nfsr),FUN=var)$x
 
 # Gráficos -----
 library(ggplot2)
+source("/Users/lbraschi/Documents/R Cosas/multiplot.R")
 
 clastcorr.plot <- ggplot(r.corrclast,aes(x=corr_teo,y=corr_emp))
 clastcorr.plot <- clastcorr.plot + 
@@ -52,7 +66,7 @@ clastcorr.pow <- clastcorr.pow +
   geom_line()+
   theme_bw()+
   scale_x_continuous(name="Correlación teórica")+
-  scale_y_continuous(name="Proporción empírica de rechazos")+
+  scale_y_continuous(name="Proporción empírica de rechazos",limits=c(0,1))+
   scale_shape_discrete(name="N fijo")+
   scale_color_discrete(name="N fijo")+
   scale_linetype_discrete(name="N fijo")+
@@ -60,9 +74,52 @@ clastcorr.pow <- clastcorr.pow +
 clastcorr.pow
 ggsave(plot=clastcorr.pow, "/Users/lbraschi/Documents/Investigación/Tesis/simulacion_correlaciones/corpowclast.png",height=8, width=12,dpi=600)
 
-histh0clast <- ggplot(datos.corrsclast[datos.corrsclast$corr_teo==0,],aes(x=tstat,y=..density..))+
-  geom_histogram(binwidth=1/10,fill="steelblue",color="black",prb=T)+
+clastcorr.n <- ggplot(r.corrclast,aes(x=corr_teo,y=nstop,color = factor(nfsr), shape = factor(nfsr), linetype = factor(nfsr)))
+clastcorr.n <- clastcorr.n + 
+  geom_point(size = 3)+
+  geom_line(size = 1)+
   theme_bw()+
-  theme(plot.title=element_text(size=18))+
-  ggtitle("Histograma H0=VERDADERA, CLAST")
-ggsave(plot=histh0clast, "/Users/lbraschi/Documents/Investigación/Tesis/simulacion_correlaciones/histh0clast.png",height=8, width=12,dpi=600)
+  scale_x_continuous(name="Correlación teórica")+
+  scale_y_continuous(name="N stop")+
+  scale_shape_discrete(name="N fijo")+
+  scale_color_discrete(name="N fijo")+
+  scale_linetype_discrete(name="N fijo")
+
+clastcorr.n
+
+# histh0clast <- ggplot(datos.corrsclast[datos.corrsclast$corr_teo==0,],aes(x=tstat,y=..density..))+
+#   geom_histogram(binwidth=1/10,fill="steelblue",color="black",prb=T)+
+#   theme_bw()+
+#   theme(plot.title=element_text(size=18))+
+#   ggtitle("Histograma H0=VERDADERA, CLAST")
+# ggsave(plot=histh0clast, "/Users/lbraschi/Documents/Investigación/Tesis/simulacion_correlaciones/histh0clast.png",height=8, width=12,dpi=600)
+
+clastcorr.varzteo <- ggplot(r.corrclast, aes(x=corr_teo,y=varteo.zfisher, color=factor(nfsr),shape=factor(nfsr), linetype=factor(nfsr)))
+clastcorr.varzteo <- clastcorr.varzteo + 
+  geom_point(size = 3)+
+  geom_line()+
+  theme_bw()+
+  theme(plot.title = element_text(size = 18, face = "bold"))+
+  scale_x_continuous(name="Correlación teórica")+
+  scale_y_continuous(name="Varianza teórica")+
+  scale_shape_discrete(name="N fijo")+
+  scale_color_discrete(name="N fijo")+
+  scale_linetype_discrete(name="N fijo")+
+  ggtitle("Varianza de Z de Fisher\na partir de los tamaños de parada")
+
+clastcorr.varzteo
+
+clastcorr.varzemp <- ggplot(r.corrclast, aes(x=corr_teo,y=varemp.zfisher, color=factor(nfsr),shape=factor(nfsr), linetype=factor(nfsr)))
+clastcorr.varzemp <- clastcorr.varzemp + 
+  geom_point(size = 3)+
+  geom_line()+
+  theme_bw()+
+  theme(plot.title = element_text(size = 18, face = "bold"))+
+  scale_x_continuous(name="Correlación teórica")+
+  scale_y_continuous(name="Varianza empírica")+
+  scale_shape_discrete(name="N fijo")+
+  scale_color_discrete(name="N fijo")+
+  scale_linetype_discrete(name="N fijo")+
+  ggtitle("Varianza de Z de Fisher\ncalculada empíricamente")
+
+clastcorr.varzemp
